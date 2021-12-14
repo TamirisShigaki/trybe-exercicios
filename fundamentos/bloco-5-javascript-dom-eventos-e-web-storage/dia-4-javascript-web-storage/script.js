d3f7953f472cfbadb657f3e6fdf89fe042a8b46f
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-14 19:32:23 -0300

    falta alguns detalhes

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+  //Cor de fundo
   let corBody = document.querySelector("#body");
   corBody.addEventListener("change", function () {
     let bodySelecionado = corBody.selectedOptions[0];
@@ -7,6 +8,7 @@ window.onload = function () {
     localStorage.setItem("background-color", bodySelecionado.value);
   });
 
+  //Cor da fonte
   let corLetra = document.querySelector("#letra");
   corLetra.addEventListener("change", function () {
     let letraSelecionada = corLetra.selectedOptions[0];
@@ -15,21 +17,53 @@ window.onload = function () {
     localStorage.setItem("color", letraSelecionada.value);
   });
 
-  let tamFonte = document.querySelector('input[name="font-size"]');
-  tamFonte.addEventListener("change", function () {
+  //Tipo de Fonte
+  let tipoFonte = document.querySelector("#fonte");
+  tipoFonte.addEventListener("change", function () {
     let p = document.querySelector("p");
-    p.style.fontSize = `${tamFonte.value}px`;
+    p.style.fontFamily = tipoFonte;
 
-    localStorage.setItem("tam", `${tamFonte.value}px`);
+    localStorage.setItem("fonte", tipoFonte);
   });
 
+  //Tamanho da Fonte
+  let tamFonte = document.querySelector('input[name="font-size"]');
+  tamFonte.addEventListener("change", function () {
+    let paragrafo = document.querySelector("p");
+    paragrafo.style.fontSize = `${tamFonte.value}px`;
+
+    localStorage.setItem("tamLetra", `${tamFonte.value}px`);
+  });
+
+  //Espaçamento entre as linhas
+  let espacoLinha = document.querySelector('input[name="line-height"]');
+  espacoLinha.addEventListener("change", function () {
+    let linhaP = document.querySelector("p");
+    linhaP.style.lineHeight = `${espacoLinha.value}px`;
+
+    localStorage.setItem("tamLinha", `${espacoLinha.value}px`);
+  });
+
+  //Cor de fundo
   let backgroundBody = localStorage.getItem("background-color");
   document.body.style.backgroundColor = backgroundBody;
 
+  //Cor da fonte
   let letraCor = localStorage.getItem("color");
   document.body.style.color = letraCor;
 
-  let tamLetra = localStorage.getItem("tam");
+  //Tipo de Fonte
+  let tipoLetra = localStorage.getItem("fonte");
   let p = document.querySelector("p");
-  p.style.fontSize = tamLetra;
+  p.style.fontFamily = tipoLetra;
+
+  //Tamanho da Fonte
+  let tamLetra = localStorage.getItem("tamLetra");
+  let paragrafo = document.querySelector("p");
+  paragrafo.style.fontSize = tamLetra;
+
+  //Espaçamento entre as linhas
+  let linhaEspaco = localStorage.getItem("tamLinha");
+  let linhaP = document.querySelector("p");
+  linhaP.style.lineHeight = linhaEspaco;
 };
