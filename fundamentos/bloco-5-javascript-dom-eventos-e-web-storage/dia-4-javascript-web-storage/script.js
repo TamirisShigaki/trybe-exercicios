75d53cd5d3986655292b1bc61737fc3ee19d0ebf
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-15 00:07:02 -0300

    Exercicios 5.4 ok

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -19,11 +19,12 @@ window.onload = function () {
 
   //Tipo de Fonte
   let tipoFonte = document.querySelector("#fonte");
-  tipoFonte.addEventListener("change", function () {
+  tipoFonte.addEventListener("change", function (evento) {
+    let fonte = evento.target.value;
     let p = document.querySelector("p");
-    p.style.fontFamily = tipoFonte;
+    p.style.fontFamily = fonte;
 
-    localStorage.setItem("fonte", tipoFonte);
+    localStorage.setItem("tipoFonte", fonte);
   });
 
   //Tamanho da Fonte
@@ -67,3 +68,17 @@ window.onload = function () {
   let linhaP = document.querySelector("p");
   linhaP.style.lineHeight = linhaEspaco;
 };
+
+/* Aprofunde seus conhecimentos
+Imagine que você é a pessoa responsável por desenvolver uma página que servirá como um leitor de conteúdo escrito.
+Esse conteúdo escrito pode ser uma página de livro, uma reportagem de revista ou uma nota de jornal online. Para que você não tenha que pensar no conteúdo da página, utilize este link para gerar o texto para sua página.
+Até aqui, nenhuma novidade, mas essa demanda exige que você aplique preferências das pessoas leitoras na página para melhorar a experiência de leitura dessas pessoas.
+As pessoas devem ter o poder de alterar:
+Cor de fundo da tela;
+Cor do texto;
+Tamanho da fonte;
+Espaçamento entre as linhas do texto;
+Tipo da fonte ( Font family ).
+Essas preferências devem ser salvas de forma que, ao retornar à página, as preferências que foram previamente configuradas possam ser aplicadas na tela.
+Bônus
+As propriedades descritas acima são obrigatórias , mas você é livre para adicionar qualquer outra propriedade que julgar válida e que tenha como objetivo a melhora da experiência da pessoa que lê em sua página. */
