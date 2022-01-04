182818fa19a165b52c549579e8d0dbfa6b2968ed
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-04 11:50:27 -0300

    Ajujes no html e começando JS

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio/script.js
@@ -1,3 +1,52 @@
+window.onload = function () {
+  function criaEstados() {
+    let estados = document.querySelector("#estados");
+    let todosEstados = [
+      "Acre",
+      "Alagoas",
+      "Amapá",
+      "Amazonas",
+      "Bahia",
+      "Ceará",
+      "Distrito Federal",
+      "Espírito Santo",
+      "Goiás",
+      "Maranhão",
+      "Mato Grosso",
+      "Mato Grosso do Sul",
+      "Minas Gerais",
+      "Pará",
+      "Paraíba",
+      "Paraná",
+      "Pernambuco",
+      "Piauí",
+      "Rio de Janeiro",
+      "Rio Grande do Norte",
+      "Rio Grande do Sul",
+      "Rondônia",
+      "Roraima",
+      "Santa Catarina",
+      "São Paulo",
+      "Sergipe",
+      "Tocantins",
+    ];
+
+    for (let index = 0; index < todosEstados.length; index += 1) {
+      let cria = document.createElement("option");
+      cria.innerHTML = todosEstados[index];
+
+      estados.appendChild(cria);
+    }
+  }
+  criaEstados();
+
+  let botaoEnviar = document.querySelector("#enviar");
+  botaoEnviar.addEventListener("click", enviarDados);
+
+  let botaoLimpar = document.querySelector("#limpar");
+  botaoLimpar.addEventListener("click", limparCampos);
+};
+
 /* Logo abaixo do formulário, crie um botão que:
 Chame uma função JavaScript e interrompa o fluxo automático do form utilizando o preventDefault() . Note que isso vai impedir as validações do HTML ao fazer o submit
 Implemente, agora, no Javascript , as validações que foram pedidas ao longo da montagem do formulário.
