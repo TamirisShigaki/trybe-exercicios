4ca792b20fb598272e4fb11b6fe140b22fe01131
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-03 21:41:16 -0300

    Add comentarios

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/fixando/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/fixando/script.js
@@ -21,3 +21,8 @@ function terceira(evento3) {
 HREF_LINK.addEventListener("click", primeira);
 INPUT_CHECKBOX.addEventListener("click", segunda);
 INPUT_TEXT.addEventListener("keypress", terceira);
+
+/* Para fixar:
+1 - Adicione um evento click no elemento a que possui o id href . Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer.
+2 - Adicione um evento click no elemento checkbox que possui o id input-checkbox . Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer.
+3 - Adicione um evento keypress no elemento input type="text" que possui o id input-text . Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra 'a' minúscula) possa ser digitado na caixinha. ( dica: para capturar a tecla digitada, utilize o event.key ) */
