71fcf9922e325b027195a099d0d6a3ca578c8c60
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-03 22:09:19 -0300

    iniciando os exercicios do dia 6.1

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio/script.js
@@ -0,0 +1,6 @@
+/* Logo abaixo do formulário, crie um botão que:
+Chame uma função JavaScript e interrompa o fluxo automático do form utilizando o preventDefault() . Note que isso vai impedir as validações do HTML ao fazer o submit
+Implemente, agora, no Javascript , as validações que foram pedidas ao longo da montagem do formulário.
+Caso todos os dados sejam válidos, monte uma <div> com o consolidado dos dados que foram inseridos no formulário.
+Caso haja algum dado inválido, mostre em uma <div> uma mensagem de erro. Se o erro for na Data de Início , a mensagem deve ser contextualizada.
+Crie um botão Limpar que limpa todos os campos do formulário e a <div> com seu currículo também. */
