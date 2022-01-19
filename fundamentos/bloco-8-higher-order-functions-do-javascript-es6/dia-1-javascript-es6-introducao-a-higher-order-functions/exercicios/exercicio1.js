ed982b014c34c8ab29613d36e69b84c445cf75a3
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-19 15:13:44 -0300

    falta a parte3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicio1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicio1.js
@@ -1,5 +1,4 @@
-// !1 - função que retorne um objeto no formato { nomeCompleto, email }. A função recebe como parâmetro o nome completo e a partir dele gera automaticamente um email no formato nome_da_pessoa@trybe.com.
-
+//! 1
 //.toLowerCase() - faz as letra maiusculas, ficarem minusculas
 //.split('') em cada espaço inclui _ com o .join('_') 
 
@@ -19,7 +18,7 @@
 
     console.log(newEmployees(novoFuncionario));
 
-//! 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+//! 2 
 // Math.ceil arredonda o num para cima,
 // Math.random sortei um num aleatorio.
 
@@ -30,5 +29,8 @@ const resultado = (num2) => sorteio(num2) ? "Parabéns, você ganhou!" : "Tente 
 console.log(resultado(3));
 
 
-//! 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
-//* Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+//! 3
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
