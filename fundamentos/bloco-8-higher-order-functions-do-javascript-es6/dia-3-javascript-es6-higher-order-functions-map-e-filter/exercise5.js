12dcac2d13221040635ea532d6d2f8acc80502be
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-20 19:06:38 -0300

    exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise5.js
@@ -61,4 +61,15 @@ const books = [
     },
   ];
 
-  // Adicione o código do exercício aqui:
+  //5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+  
+  const expectedResult = [
+    'Frank Herbert',
+    'George R. R. Martin',
+    'Isaac Asimov',
+    'J. R. R. Tolkien',
+  ];
+  
+  function fantasyOrScienceFictionAuthors() {
+    // escreva seu código aqui
+  }
