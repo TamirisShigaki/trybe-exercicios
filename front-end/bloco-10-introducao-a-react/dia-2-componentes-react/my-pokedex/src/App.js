5f3f6a8805a53052108573d4d4d36db497a1d197
Tamiris Shigaki <shigakiendo@gmail.com>
2022-02-18 10:56:44 -0300

    add mais css

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   render () {
     return (
       <div className='main'>
+        <link rel="shortcut icon" href="./public/pokebola.jpg" type="image/x-icon"/> 
       <h1>POKEDEX</h1>
       <Pokedex />
       </div>
