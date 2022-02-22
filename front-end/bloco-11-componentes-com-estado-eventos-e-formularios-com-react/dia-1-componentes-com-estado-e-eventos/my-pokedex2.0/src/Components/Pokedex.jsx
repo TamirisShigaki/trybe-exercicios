6be393230d817062801b7a8a631f183ab424aa75
Tamiris Shigaki <shigakiendo@gmail.com>
2022-02-22 11:14:24 -0300

    exercicio 2: filtrando os pokemons por type e ajustando o css

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-pokedex2.0/src/Components/Pokedex.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-pokedex2.0/src/Components/Pokedex.jsx
@@ -3,28 +3,52 @@ import Pokemon from './Pokemon';
 import pokemons from '../data';
 
 class Pokedex extends React.Component {
-    constructor() {
+    constructor(){
         super()
         this.state = {
-            indexPokemon: 0
+            indexPokemon: 0,
+            pokemonType: 'all'
         }
-        this.allPokemons = this.allPokemons.bind(this)
+
+        this.nextPokemon = this.nextPokemon.bind(this)
+        this.listPokemon = this.listPokemon.bind(this)
+        this.filterPokemon = this.filterPokemon.bind(this)
+        this.typePokemon = this.typePokemon.bind(this)
     }
 
-    allPokemons (size) {
-        this.setState((actual, _props) => ({indexPokemon: (actual.indexPokemon +=1) % size}))
+    nextPokemon () {
+       this.setState((atual, _props) => ({
+            indexPokemon: (atual.indexPokemon +=1) % this.listPokemon().length,
+        }))
     }
-    render (){
-        return (
-            <div>
-            <div className='container'>
-                {
-                    pokemons.map((objPokemon) => 
-                    <Pokemon key={ objPokemon.id } getPokemon={ objPokemon }/>
-                    )[this.state.indexPokemon]
-                }
-            </div>
-                <button onClick={() => this.allPokemons(pokemons.length)}>All</button>
+
+    listPokemon () {
+        return pokemons.filter((filtraObjeto) => this.state.pokemonType !== 'all' ? filtraObjeto.type === this.state.pokemonType : filtraObjeto).map((objeto) => <Pokemon key={objeto.id} getPokemon={ objeto} />)
+    }
+
+    filterPokemon (event) {
+        this.setState((atual, _props) => ({
+            pokemonType: atual.pokemonType = event.target.value,
+            indexPokemon: atual.indexPokemon = 0
+        }))
+    }
+
+    typePokemon () {
+        return [...new Set(pokemons.map(({ type }) => type))]
+    }
+
+    render(){
+        return(
+            <div className='conteiner-all'>
+                <section className='container'>
+                    {this.listPokemon()[this.state.indexPokemon]}
+                </section>
+                <button onClick={this.filterPokemon} value="all" className='all' >All</button>
+                    {this.typePokemon().map((type) => {
+                        return <button key={type} onClick={this.filterPokemon} value={type} className={type} >{type}</button>
+                    })}
+                    <br />
+                    <button onClick={this.nextPokemon}  disabled={this.listPokemon().length <= 1} className='next' >Next Pokemon</button>
             </div>
         )
     }
